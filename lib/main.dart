602e5304290ed7d870ecaa7d69545e0f2459506a
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2020-02-16 12:59:43 +0530

    Change UI using Themes

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Namesake: A startup name suggestor',
+      theme: ThemeData(
+        primaryColor: Colors.lightBlueAccent
+      ),
       home: RandomWords()
     );
   }
